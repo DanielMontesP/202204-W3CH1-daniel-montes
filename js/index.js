533db7324158d3a8b79efa4fca99427a14e9e1cd
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-19 04:29:26 +0200

    Add html UI

--- js/index.js
+++ js/index.js
@@ -1,24 +1,7 @@
-import Asesor from "./Asesor.js";
-import Escudero from "./Escudero.js";
-import Luchador from "./Luchador.js";
-import Personaje from "./Personaje.js";
-import Rey from "./Rey.js";
 import ButtonComponent from "../components/ButtonComponent.js";
 
-const container = document.querySelector(".container");
+const container = document.querySelector("body");
 
-const startButton = new ButtonComponent(container, "Winter is coming");
-
-export const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
-export const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
-export const daenerys = new Luchador(
-  "Daenerys",
-  "Targaryen",
-  30,
-  "Dragones",
-  10
-);
-export const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
-export const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
+const startButton = new ButtonComponent(container, "Show Cards");
 
 // console.log(ListaPersonajes);
--- js/personajes.js
+++ js/personajes.js
@@ -1,4 +1,15 @@
-import { bronn, daenerys, jaime, joffrey, tyrion } from "./index.js";
+// import { bronn, daenerys, jaime, joffrey, tyrion } from "./index.js";
+import Asesor from "./Asesor.js";
+import Escudero from "./Escudero.js";
+import Luchador from "./Luchador.js";
+import Personaje from "./Personaje.js";
+import Rey from "./Rey.js";
+
+const joffrey = new Rey("Joffrey", "Baratheon", 16, 2);
+const jaime = new Luchador("Jaime", "Lannister", 45, "Espada", 7);
+const daenerys = new Luchador("Daenerys", "Targaryen", 30, "Dragones", 10);
+const tyrion = new Asesor("Tyrion", "Lannister", 40, daenerys);
+const bronn = new Escudero("Bronn", "AguasNegras", 54, 0, jaime);
 
 export function createLista() {
   const ListaPersonajes = [joffrey, daenerys, jaime, tyrion, bronn];
